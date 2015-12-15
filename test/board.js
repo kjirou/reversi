dfa54d46464bf0aa9c695e2779736f4456cb10e4
kjirou <kjirou.web@gmail.com>
2015-12-16 02:31:44 +0900

    Refactor codes / Add tests

---
+++ test/board.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+
+var Board = require('../lib/board');
+
+
+describe('lib/board', function() {
+
+  it('_createSquares', function() {
+    var squares = Board.prototype._createSquares(2, 3);
+    assert.strictEqual(squares.length, 2);
+    assert.strictEqual(squares[0].length, 3);
+    assert.strictEqual(typeof squares[0][0], 'object');
+  })
+
+  it('new', function() {
+    var board = new Board();
+    assert.strictEqual(board._squares.length, 8);
+    assert.strictEqual(board._squares[0].length, 8);
+  });
+
+  it('initializeGame', function() {
+    var board = new Board();
+    board.initializeGame();
+    assert.strictEqual(board.toText(), [
+      '--------',
+      '--------',
+      '--------',
+      '---ox---',
+      '---xo---',
+      '--------',
+      '--------',
+      '--------'
+    ].join('\n'));
+  });
+
+  it('_tryToReverseTowardOneDirection, tryToReverse', function() {
+    var board = new Board();
+    board.initializeGame();
+    board._putPiece(3, 2, Board.PIECE_TYPES.WHITE);
+    board._putPiece(4, 0, Board.PIECE_TYPES.WHITE);
+    board._putPiece(4, 1, Board.PIECE_TYPES.BLACK);
+    assert.strictEqual(board.toText(), [
+      '--------',
+      '--------',
+      '--------',
+      '--oox---',
+      'ox-xo---',
+      '--------',
+      '--------',
+      '--------'
+    ].join('\n'));
+
+    assert.deepEqual(board._tryToReverseTowardOneDirection(3, 1, Board.PIECE_TYPES.BLACK, [0, 1]), [
+      [3, 2],
+      [3, 3]
+    ]);
+    assert.deepEqual(board._tryToReverseTowardOneDirection(3, 1, Board.PIECE_TYPES.WHITE, [0, 1]), []);
+    assert.deepEqual(board._tryToReverseTowardOneDirection(3, 1, Board.PIECE_TYPES.BLACK, [1, 1]), []);
+
+    assert.deepEqual(board._tryToReverseTowardOneDirection(4, 2, Board.PIECE_TYPES.WHITE, [0, 1]), [
+      [4, 3]
+    ]);
+    assert.deepEqual(board._tryToReverseTowardOneDirection(4, 2, Board.PIECE_TYPES.WHITE, [0, -1]), [
+      [4, 1]
+    ]);
+    assert.deepEqual(board._tryToReverseTowardOneDirection(4, 2, Board.PIECE_TYPES.WHITE, [1, 0]), []);
+    assert.deepEqual(board._tryToReverseTowardOneDirection(4, 2, Board.PIECE_TYPES.BLACK, [0, 1]), []);
+
+    assert.deepEqual(board.tryToReverse(3, 1, Board.PIECE_TYPES.BLACK), [
+      [3, 2],
+      [3, 3]
+    ]);
+    assert.deepEqual(board.tryToReverse(4, 2, Board.PIECE_TYPES.WHITE), [
+      [4, 3],
+      [4, 1]
+    ]);
+  });
+});

2517dddf8a047ea7c0d0d7aed898977d3284ebdc
kjirou <sorenariblog@gmail.com>
2015-12-15 19:37:23 +0900

    Write architectural development

---
+++ index.js
@@ -0,0 +1,91 @@
+function Board() {
+
+  this._squares = [];
+
+  for (var rowIndex = 0; rowIndex < 8; rowIndex += 1) {
+    var squaresOfRow = [];
+    for (var colIndex = 0; colIndex < 8; colIndex += 1) {
+      (function(rowIndex, colIndex) {
+        squaresOfRow.push({
+          rowIndex: rowIndex,
+          colIndex: colIndex,
+          komaType: null  // or 'white', 'black'
+        });
+      })(rowIndex, colIndex);
+    }
+    this._squares.push(squaresOfRow);
+  }
+
+  this.putKoma(3, 3, 'white');
+  this.putKoma(3, 4, 'black');
+  this.putKoma(4, 4, 'white');
+  this.putKoma(4, 3, 'black');
+}
+
+Board.prototype.putKoma = function(rowIndex, colIndex, komaType) {
+  this._squares[rowIndex][colIndex].komaType = komaType;
+};
+
+Board.prototype.tryReverseOthers = function(rowIndex, colIndex, komaType) {
+  var that = this;
+  var directions = [
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1]
+  ];
+  var reversibleSquares = [];
+  directions.forEach(function(direction) {
+    reversibleSquares = reversibleSquares.concat(
+      that.tryReverseToDirection(rowIndex, colIndex, komaType, direction)
+    );
+  });
+  return reversibleSquares;
+};
+
+Board.prototype.tryReverseToDirection = function(rowIndex, colIndex, fromKomaType, direction) {
+  var that = this;
+  var reversibleSquares = [];
+  function nextStep(rowIndex, colIndex, fromKomaType, direction) {
+    var nextSquare = that._squares[rowIndex][colIndex];
+    if (
+      !nextSquare &&
+      nextSquare.komaType !== null &&
+      nextSquare.komaType !== fromKomaType
+    ) {
+      reversibleSquares.push([rowIndex, colIndex]);
+      nextStep(rowIndex + direction[0], colIndex + direction[1], fromKomaType, direction);
+    }
+  };
+  nextStep(rowIndex, colIndex, fromKomaType, direction);
+  return reversibleSquares;
+};
+
+Board.prototype.canReverseOthers = function(rowIndex, colIndex, komaType) {
+};
+
+Board.prototype.toText = function() {
+  return this._squares.map(function(squaresOfRow) {
+    return squaresOfRow.map(function(square) {
+      var komaText = ' ';
+      if (square.komaType === 'white') {
+        komaText = 'o';
+      } else if (square.komaType === 'black') {
+        komaText = 'x';
+      }
+      return komaText;
+    }).join('');
+  }).join('\n');
+};
+
+
+var board = new Board();
+
+console.log(board.toText());
+
+var result = board.tryReverseOthers(2, 3, 'black');
+console.log(result);

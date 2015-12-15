1d4bde75517388fcd5cfd7f94d7f0da56d03cf64
kjirou <kjirou.web@gmail.com>
2015-12-16 03:24:44 +0900

    Define init command

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var Board = require('./lib/board');
+var storage = require('./lib/storage');
+
+
+function executeInitCommand(callback) {
+  storage.saveInitialData(callback);
+}
+
+module.exports = {
+  executeInitCommand: executeInitCommand
+};

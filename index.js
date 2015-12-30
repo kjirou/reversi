a9c321adfcda133f01f88895de53c978791f38b5
kjirou <kjirou.web@gmail.com>
2015-12-30 18:00:37 +0900

    Provide inner classes to outer now

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const board = require('./lib/board');
 const consts = require('./lib/consts');
 const game = require('./lib/game');
+const square = require('./lib/square');
 
 
 module.exports = {
   PIECE_TYPES: consts.PIECE_TYPES,
   PRESET_RULE_TYPES: consts.PRESET_RULE_TYPES,
+  Board: board.Board,
   Game: game.Game,
+  Square: square.Square,
 };
